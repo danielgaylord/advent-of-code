9eb2b2ebec83e395278bcff407a4ec7dea3c7a64
Dan Gaylord <danielgaylord@gmail.com>
2022-12-05 22:59:32 -0500

    Day 4 Done

--- 2022/Day 4 - Camp Cleanup/code.py
+++ 2022/Day 4 - Camp Cleanup/code.py
@@ -2,15 +2,31 @@ import unittest, os
 
 # Read puzzle input and return as usable data structure
 def parse_input(file):
-    rucksacks = []
+    pairs = []
     with open(os.path.join(os.path.dirname(__file__), file), 'r') as input:
-        rucksacks = [line.rstrip() for line in input]
-    return rucksacks
+        pairs = [line.rstrip().split(",") for line in input]
+    return pairs
 
 def core(file):
-    rucksacks = parse_input(file)
+    pairs = parse_input(file)
 
-    return prioritySum
+    # Part 1: Only consider fully contains
+    overlaps = 0
+    for pair in pairs:
+        low1, high1 = [int(num) for num in pair[0].split("-")]
+        low2, high2 = [int(num) for num in pair[1].split("-")]
+        if (low1 <= low2 and high1 >= high2) or (low1 >= low2 and high1 <= high2):
+            overlaps += 1
+
+    # Part 2: Any overlaps matter
+    overlaps = 0
+    for pair in pairs:
+        low1, high1 = [int(num) for num in pair[0].split("-")]
+        low2, high2 = [int(num) for num in pair[1].split("-")]
+        if (low1 <= low2 and high1 >= high2) or (low1 >= low2 and high1 <= high2) or (high1 >= low2 and high1 <= high2) or (low1 >= low2 and low1 <= high2):
+            overlaps += 1
+
+    return overlaps
 
 # Unit testing on given test input and expected results
 class TestCases(unittest.TestCase):
@@ -18,16 +34,16 @@ class TestCases(unittest.TestCase):
         self.assertEqual(core('example.txt'), 2)
     
     def test_part2(self):
-        self.assertEqual(core('example.txt'), 0)
+        self.assertEqual(core('example.txt'), 4)
 
 if __name__ == "__main__":
     # unittest.main(verbosity=2)
     
     # Part 1 solution
-    print(core('example.txt'))
+    # print(core('example.txt'))
     # print(core('input.txt'))
     
     # Part 2 solution
     # print(core('example.txt'))
-    # print(core('input.txt'))
+    print(core('input.txt'))
         
